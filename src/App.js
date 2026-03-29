74c9ff2c6689e16898317ca47f11487c193750b0
Kobiche <koba156@yandex.ru>
2026-03-29 23:53:10 +0300

    homework 1 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import itemBlue from './item-blue.jpg';
-import itemBlack from './item-black.jpg';
+import itemBlue from './img/item-blue.jpg';
+import itemBlack from './img/item-black.jpg';
 import './App.css';
 
 const item = {
@@ -13,6 +13,22 @@ const item = {
 
 function ShopItemFunc(props){
   
+  return(
+    <div className="main-content">
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">
+        {props.item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{props.item.currency}{props.item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 }
 
 function App() {
@@ -27,24 +43,6 @@ function App() {
       <ShopItemFunc item={item} />
     </div>
   </div>
-    
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-
   );
 }
 
